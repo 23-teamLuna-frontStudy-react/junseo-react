047cfab09d990fc384188e150c5c0d5ab691c959
박준서 <jswing5267@naver.com>
2023-09-08 21:36:37 +0900

    typescript 연습 완료 버전

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import Circle from "./Circle";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+  height: 100vh;
+  width: 100vw;
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            value={value}
+            onChange={onChange}
+            type="text"
+            placeholder="username"
+          ></input>
+          <button>Log in</button>
+        </form>
+      </div>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
