ff1498fffa69c3f3b7f184614fc361348863c1ba
박준서 <jswing5267@naver.com>
2023-09-28 13:59:12 +0900

    9.3강

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import TV from "./Routes/TV";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<TV />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 export default App;
---
+++ src/Routes/TV.tsx
@@ -0,0 +1,4 @@
+function TV() {
+  return <h1>Tv</h1>;
+}
+export default TV;
